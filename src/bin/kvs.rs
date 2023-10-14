98309bdab39b118afefc15478e6fc714d789e491
RamezesDong <donghouze666@outlook.com>
2023-10-14 20:04:22 +0800

    finish rust-proj1

---
+++ src/bin/kvs.rs
@@ -0,0 +1,46 @@
+use clap::{Arg, Command};
+use std::process::exit;
+
+fn main() {
+    let matches = Command::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            Command::new("get")
+                .about("Get the string value of a given string key")
+                .arg(Arg::new("KEY").help("A string key").required(true)),
+        )
+        .subcommand(
+            Command::new("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::new("KEY").help("A string key").required(true))
+                .arg(
+                    Arg::new("VALUE")
+                        .help("The string value of the key")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            Command::new("rm")
+                .about("Remove a given key")
+                .arg(Arg::new("KEY").help("A string key").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("set", _sub_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Some(("get", _sub_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Some(("rm", _sub_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
+}
